1b27ece6780b8b1ce731eb240989c23bcdb65d9f
PSBigBig <hello@onestardao.com>
2025-06-10 17:14:26 +0800

    Update __init__.py

--- wfgy_sdk/__init__.py
+++ wfgy_sdk/__init__.py
@@ -1,3 +1,9 @@
 # wfgy_sdk/__init__.py
 
-__version__ = "1.0.0"
+def enable(model):
+    print("WFGY 1.0 enabled on model:", model)
+    return model
+
+def disable(model):
+    print("WFGY 1.0 disabled on model:", model)
+    return model
