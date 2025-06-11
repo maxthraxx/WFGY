474258c5c2347d27fd2f598572e66523ac2e5083
PSBigBig <hello@onestardao.com>
2025-06-11 14:45:00 +0800

    Update example_02_self_reflection.py

--- example_02_self_reflection.py
+++ example_02_self_reflection.py
@@ -1,14 +1,11 @@
-import argparse
+# example_02_self_reflection.py
 from wfgy_core import WFGYRunner
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--reflect", action="store_true", help="Enable self-reflection")
-    parser.add_argument("--ascii", action="store_true", help="Show ASCII banner")
-    parser.add_argument("--style", type=str, default="default", help="Choose style: default/scientific/meme")
-
-    args = parser.parse_args()
-
-    prompt = "Why don't AIs like to take showers?"
     runner = WFGYRunner()
-    runner.run(prompt, reflect=args.reflect, style=args.style, show_ascii=args.ascii)
+    runner.run(
+        prompt="Why don't AIs like to take showers?",
+        reflect=True,
+        style="meme",
+        show_ascii=True
+    )
